511faa631cc742b3067dd88c70cc72f3e13f7da3
Alexander Lukyanov <lux-nova@mail.ru>
2022-02-04 00:41:51 +0300

    Fix commit

--- src/main/java/ru/netology/conditionalspring/config/Config.java
+++ src/main/java/ru/netology/conditionalspring/config/Config.java
@@ -11,13 +11,13 @@ import ru.netology.conditionalspring.model.SystemProfile;
 public class Config {
 
     @Bean
-    @ConditionalOnProperty(name = "netology.profile.dev = true", havingValue = "true", matchIfMissing = true)
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true", matchIfMissing = true)
     public SystemProfile devProfile() {
         return new DevProfile();
     }
 
     @Bean
-    @ConditionalOnProperty(name = "netology.profile.dev = false", havingValue = "false")
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false")
     public SystemProfile prodProfile() {
         return new ProductionProfile();
     }
