dde21d19b7e74193f6b3645c08043b7651d08198
Alexander Lukyanov <lux-nova@mail.ru>
2022-04-14 14:42:35 +0300

    testy containers come

--- src/test/java/ru/netology/conditionalspring/ConditionalSpringApplicationTests.java
+++ src/test/java/ru/netology/conditionalspring/ConditionalSpringApplicationTests.java
@@ -1,13 +1,30 @@
 package ru.netology.conditionalspring;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ConditionalSpringApplicationTests {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+    private static final GenericContainer<?> devapp = new GenericContainer<>("devapp").withExposedPorts(8080);
+    private static final GenericContainer<?> prodapp = new GenericContainer<>("prodapp").withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+
+    }
 
     @Test
     void contextLoads() {
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + devapp.getMappedPort(8080), String.class);
+        System.out.println(forEntity.getBody());
     }
 
 }
