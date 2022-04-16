ca6789f445258abb11051377491a61a34a1c9349
Alexander Lukyanov <lux-nova@mail.ru>
2022-04-16 15:28:00 +0300

    testy pass

--- src/test/java/ru/netology/conditionalspring/ConditionalSpringApplicationTests.java
+++ src/test/java/ru/netology/conditionalspring/ConditionalSpringApplicationTests.java
@@ -7,24 +7,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+
+@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ConditionalSpringApplicationTests {
 
     @Autowired
     TestRestTemplate restTemplate;
+    @Container
     private static final GenericContainer<?> devapp = new GenericContainer<>("devapp").withExposedPorts(8080);
+    @Container
     private static final GenericContainer<?> prodapp = new GenericContainer<>("prodapp").withExposedPorts(8081);
 
-    @BeforeAll
-    public static void setUp() {
-
-    }
+//    @BeforeAll
+//    public static void setUp() {
+//
+//    }
 
     @Test
     void contextLoads() {
-        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:" + devapp.getMappedPort(8080), String.class);
-        System.out.println(forEntity.getBody());
+        ResponseEntity<String> forEntityDev = restTemplate.getForEntity("http://localhost:" + devapp.getMappedPort(8080), String.class);
+        System.out.println(forEntityDev.getBody());
+
+        ResponseEntity<String> forEntityProd = restTemplate.getForEntity("http://localhost:" + prodapp.getMappedPort(8080), String.class);
+        System.out.println(forEntityProd.getBody());
+
     }
 
 }
